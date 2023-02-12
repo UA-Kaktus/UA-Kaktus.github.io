b8d6e685768bc61f95b14a663eefc4630b99e4d6
UA-Kaktus <101982234+UA-Kaktus@users.noreply.github.com>
2023-02-12 17:34:11 +0200

    Add files via upload

--- brooke/js/main.js
+++ brooke/js/main.js
@@ -70,41 +70,88 @@ document.addEventListener("DOMContentLoaded",()=> {
         }
     });
 
-    //Слайдер
-    const slider = document.querySelector('.news__slider');
+    //Слайдер 2.0
+
     const sliderWrapper = document.querySelector('.slider__wrapper');
     const slides = document.querySelectorAll('.slider__wrapper .slider__item');
+    const controllersWrap = document.querySelector('.slider__nav .slider__nav-list');
+    let controllers = [];
+    let activeSlide = 1;
 
-    let slideWith = window.getComputedStyle(slides[0]).width;
-    let sliderWrapperWidth = slides.length * (deleteNotDigits(slideWith) + 20) - 20;
+    const slideWith = window.getComputedStyle(slides[0]).width;
+    const sliderWrapperWidth = slides.length * (deleteNotDigits(slideWith) + 20) - 20;
 
     sliderWrapper.style.width = `${sliderWrapperWidth}px`;
     sliderWrapper.style.display = `flex`;
 
-    let test = window.innerWidth;
-    console.log(test);
-
-   const controllers = document.querySelector('.slider__nav .slider__nav-list');
-   let offset = 0;
-   let slydesPerSwipe = 1;
-   let howManyDotsDelete = window.innerWidth > 1200 ? 2 : 1;
-   let dotQuantity = slides.length / slydesPerSwipe - howManyDotsDelete;
-   for (let i = 0; i < dotQuantity; i++) {
-        let listItem = document.createElement('li');
-        i == 0 ? listItem.classList.add('active-dot') : null;
-        listItem.addEventListener('click', (e) => {
-            offset = (deleteNotDigits(slideWith) + 20) * slydesPerSwipe * i;
-            sliderWrapper.style.left = `-${offset}px`;
-            let dots = document.querySelectorAll('.slider__nav .slider__nav-list li');
-            dots.forEach(el => {
-                el.classList.remove('active-dot');
-            });
+    for (let i = 0; i < slides.length; i++) {
+        let dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.addEventListener('click', (e) => {
+            controllers.forEach(el => el.classList.remove('active-dot'));
             e.target.classList.add('active-dot');
-        });
-        controllers.append(listItem);
-   }
+            activeSlide = e.target.getAttribute('data-slide-to');
+            let offset = (activeSlide - 1) * (deleteNotDigits(slideWith) + 20);
+            sliderWrapper.style.left = `-${offset}px`;
+        })
+        controllers.push(dot);
+    }
 
-   function deleteNotDigits(str) {
+    let visibleDots = controllers.length - (window.innerWidth > 1200 ? 2 : 1);
+
+    controllers.forEach(el => {
+        if (el.getAttribute('data-slide-to') <= visibleDots) {
+            controllersWrap.append(el);
+        }
+    });
+    controllers[0].classList.add('active-dot');
+
+    sliderWrapper.addEventListener('touchstart', handleTouchStart, false);  
+    sliderWrapper.addEventListener('touchmove', handleTouchMove, false);
+
+    let xDown = null;                                                        
+    let yDown = null;                                                        
+
+    function handleTouchStart(evt) {                                         
+        xDown = evt.touches[0].clientX;                                      
+        yDown = evt.touches[0].clientY;                                      
+    };                                                
+
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        let xUp = evt.touches[0].clientX;                                    
+        let yUp = evt.touches[0].clientY;
+
+        let xDiff = xDown - xUp;
+        let yDiff = yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+            if ( xDiff > 0 ) {
+                if (activeSlide < visibleDots) {
+                    activeSlide++
+                    controllers.forEach(el => el.classList.remove('active-dot'));
+                    controllers[activeSlide-1].classList.add('active-dot');
+                    let offset = (activeSlide - 1) * (deleteNotDigits(slideWith) + 20);
+                    sliderWrapper.style.left = `-${offset}px`;
+                }
+            } else {
+                if (activeSlide > 1) {
+                    activeSlide--
+                    controllers.forEach(el => el.classList.remove('active-dot'));
+                    controllers[activeSlide-1].classList.add('active-dot');
+                    let offset = (activeSlide - 1) * (deleteNotDigits(slideWith) + 20);
+                    sliderWrapper.style.left = `-${offset}px`;                    
+                }
+            }
+        }
+        xDown = null;
+        yDown = null;                                             
+    };
+
+    function deleteNotDigits(str) {
         return +str.replace(/\D/g, '');
     }
 
